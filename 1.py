036001aab1e06157726bf93cebc3d25c98e5669d
ChuWeiChang <chuweichang0319@gmail.com>
2025-11-29 20:40:23 +0800

    Enhance plotting function by adding comments and improving code readability

--- 1.py
+++ 1.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot(config, time_all, longitudinal, transverse):
-
     x_ticks = [t * config["tr"] for t in range(0, config["duration"] + 2)]
 
     plt.figure()
+
+    # you may comment/uncomment one the following two lines to show the individual plots
     plt.plot(time_all, longitudinal, label="M_z (Longitudinal)", color="blue")
     plt.plot(time_all, transverse, label="M_xy (Transverse)", color="red")
 
@@ -14,8 +15,8 @@ def plot(config, time_all, longitudinal, transverse):
         ['–TR', '0'] + [f'{i}TR' for i in range(1, len(x_ticks) - 1)]
     )
     plt.xlabel("Time")
-
     plt.ylabel("Magnetization / M0")
+
     plt.title("Magnetization (T1 & T2)")
     plt.grid(True)
     plt.legend()
@@ -26,6 +27,7 @@ def calc_magnetization(config):
     t1_r = lambda x: m * (1 - np.exp(-x / config["t1"]))
     t2_r = lambda x: m * np.exp(-x / config["t2"])
 
+    # initial time samples for the first [-TR, 0]  interval
     time_all = list(np.linspace(0, config["tr"], config["sample_num"]))
     longitudinal = [1] * config["sample_num"]
     transverse = [0] * config["sample_num"]
@@ -47,8 +49,8 @@ def main():
         "t1": 1300,
         "t2": 80,
         "tr": 1000,
-        "M0": 3,
-        "sample_num": 100
+        "M0": 3, # doesn't affect the normalized plot
+        "sample_num": 100 # for smooth curves
     }
     calc_magnetization(config)
 
