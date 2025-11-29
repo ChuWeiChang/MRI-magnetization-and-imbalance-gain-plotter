1e58bcbf182cb0efa89640c8d8c20d5e4384681e
ChuWeiChang <chuweichang0319@gmail.com>
2025-11-29 19:24:49 +0800

    Rename variable tau_all to time_all for clarity in plotting function

--- 1.py
+++ 1.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(config, tau_all, mag_all):
+def plot(config, time_all, mag_all):
     x_ticks = [t * config["tr"] for t in range(0, config["duration"] + 2)]
-    plt.plot(tau_all, mag_all)
+    plt.plot(time_all, mag_all)
     plt.xticks(
         x_ticks,
         ['–TR', '0'] + [f'{i}TR' for i in range(1, len(x_ticks) - 1)]
@@ -19,21 +19,19 @@ def calc_longitudinal(config):
     m = config["M0"]
     t1_r = lambda x: m * (1 - np.exp(-x / config["t1"]))
 
-    tau_all = list(np.linspace(0, config["tr"], config["sample_num"]))
+    time_all = list(np.linspace(0, config["tr"], config["sample_num"]))
     mag_all = [1] * config["sample_num"]
     for t in range(0, config["duration"]):
-        tau = np.linspace(0, config["tr"], config["sample_num"])
-        mag = t1_r(tau)
-        tau_all.extend(list(tau + (t+1) * config["tr"]))
+        time = np.linspace(0, config["tr"], config["sample_num"])
+        mag = t1_r(time)
+        time_all.extend(list(time + (t+1) * config["tr"]))
         mag_all.extend(mag / config["M0"])
         m = mag[-1]
-    plot(config, tau_all, mag_all)
-
-
-
+    plot(config, time_all, mag_all)
 
 def calc_transverse(config):
     pass
+
 def main():
     config = {
         "duration": 4,
